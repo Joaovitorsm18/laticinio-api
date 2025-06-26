c1fee1c9a3e72b0d24a6188f3f7e2337d1e097b6
João Vítor de Sousa Menezes <jaovit81@gmail.com>
2025-06-26 20:46:45 -0300

    adicionando ao painel de admin

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from sales.models import Sale, SaleItem
 
-# Register your models here.
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer','date', 'status', 'receipt_photo', 'total')
+
+@admin.register(SaleItem)
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sale', 'product', 'unit', 'quantity', 'unit_price', 'total_price')
