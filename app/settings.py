ec763b0645ab1c0cc7126a8934b91e31668b7896
João Vítor de Sousa Menezes <jaovit81@gmail.com>
2025-06-25 23:13:19 -0300

    configurando uwsgi

--- app/settings.py
+++ app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'laticinio_api',
+        'USER': 'postgres',
+        'PASSWORD': 'jo2002',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -126,7 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://laticiniomara.vercel.app'
+]
 
 # settings.py
 REST_FRAMEWORK = {
