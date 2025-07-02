bb840a1e6a76a06f7a001a3a51662a542e723a69
João Vítor de Sousa Menezes <jaovit81@gmail.com>
2025-07-01 21:24:34 -0300

    adicionando sales em customers

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from customers.models import Customer
+from sales.serializers import SaleSerializer
 
 class CustomerSerilizer(serializers.ModelSerializer):
+    sales = SaleSerializer(many=True, read_only=True)
     
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['id', 'name', 'nickname', 'customer_type', 'sales']
